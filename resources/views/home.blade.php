94025ec9e68f849053122b2f427cabedcb2c5b89
Тамабай Ерсайын <Yersaiyn.Tamabay@THEEURASIA.LOCAL>
2020-01-23 18:14:42 +0600

    modal box home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,30 +4,123 @@
     <div class="row">
         @include('user.layout.sidebar')
         <div class="col-md-9">
-            <div class="row">
-                <div class="col-sm-6">
-                    <a href="/employee_insurance" class="landing-page-list__link landing-page-list__link_0 landing-page-list__link_1">
-                        <span class="landing-page-list__title">Страхование работников от несчастных случаев</span>
-                    </a>
+            @if(Auth::user()->status === 'admin')
+
+            @else
+
+                <h3>Наши Услуги</h3>
+                <div class="row mt-4 product-list">
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="product">
+                            <div class="product__img landing-page-list__link_1">
+                                <div class="product__img__logo">
+                                    <img src="/img/logo_1.svg" width="44" height="44">
+                                </div>
+                            </div>
+                            <div class="product__title">Страхование работников от несчастных случаев</div>
+                            <div class="product__desc link_active">Обязательное страхование работников от несчастных случаев (ОСНС).</div>
+                            <div class="product-btn-main">
+                                <button class="btn btn-success btn-login btn-sm product-btn">Заключить договор</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="product">
+                            <div class="product__img landing-page-list__link_2">
+                                <div class="product__img__logo">
+                                    <img src="/img/logo_1.svg" width="44" height="44">
+                                </div>
+                            </div>
+                            <div class="product__title">Аннуитетное страхование</div>
+                            <div class="product__desc link_active">Выплаты пострадавшему работнику или лицам, имеющим право на возмещение вреда.</div>
+                            <div class="product-btn-main">
+                                <button class="btn btn-success btn-login btn-sm product-btn">Заключить договор</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="product">
+                            <div class="product__img landing-page-list__link_3">
+                                <div class="product__img__logo">
+                                    <img src="/img/logo_1.svg">
+                                </div>
+                            </div>
+                            <div class="product__title">Страхование жизни</div>
+                            <div class="product__desc link_active">Финансовая поддержка для вашей семьи на случай непредвиденных событий.</div>
+                            <div class="product-btn-main">
+                                <button class="btn btn-success btn-login btn-sm product-btn" data-toggle="modal" data-target="#exampleModalCenter">Заключить договор</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="product">
+                            <div class="product__img landing-page-list__link_13">
+                                <div class="product__img__logo">
+                                    <img src="/img/logo_1.svg" width="44" height="44">
+                                </div>
+                            </div>
+                            <div class="product__title">Обязательное страхование туриста</div>
+                            <div class="product__desc link_active">Программа страхования предназначена для защиты от непредвиденных рисков и расходов, возникающих при поездке за рубеж по туристическому пакету.</div>
+                            <div class="product-btn-main">
+                                <button class="btn btn-success btn-login btn-sm product-btn">Заключить договор</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="product">
+                            <div class="product__img landing-page-list__link_13">
+                                <div class="product__img__logo">
+                                    <img src="/img/logo_1.svg" width="44" height="44">
+                                </div>
+                            </div>
+                            <div class="product__title">Обязательное страхование жизни</div>
+                            <div class="product__desc link_active">Программа страхования предназначена для защиты от непредвиденных рисков и расходов, возникающих при поездке за рубеж по туристическому пакету.</div>
+                            <div class="product-btn-main">
+                                <button class="btn btn-success btn-login btn-sm product-btn">Заключить договор</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="product">
+                            <div class="product__img landing-page-list__link_13">
+                                <div class="product__img__logo">
+                                    <img src="/img/logo_1.svg" width="44" height="44">
+                                </div>
+                            </div>
+                            <div class="product__title">Страхование от несчастных случаев</div>
+                            <div class="product__desc link_active">Программа страхования предназначена для защиты от непредвиденных рисков и расходов, возникающих при поездке за рубеж по туристическому пакету.</div>
+                            <div class="product-btn-main">
+                                <button class="btn btn-success btn-login btn-sm product-btn">Заключить договор</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-sm-6">
-                    <a href="/annuity_insurance" class="landing-page-list__link landing-page-list__link_0 landing-page-list__link_2">
-                        <span class="landing-page-list__title">Аннуитетное страхование</span>
-                    </a>
+                <!--
+                <div class="row">
+                    <div class="col-sm-6">
+                        <a href="/employee_insurance" class="landing-page-list__link landing-page-list__link_0 landing-page-list__link_1">
+                            <span class="landing-page-list__title">Страхование работников от несчастных случаев</span>
+                        </a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="/annuity_insurance" class="landing-page-list__link landing-page-list__link_0 landing-page-list__link_2">
+                            <span class="landing-page-list__title">Аннуитетное страхование</span>
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6">
-                    <a href="/life_insurance" class="landing-page-list__link landing-page-list__link_0 landing-page-list__link_3">
-                        <span class="landing-page-list__title">Страхование жизни</span>
-                    </a>
-                </div>
-                <div class="col-sm-6">
-                    <a href="/travel_insurance" class="landing-page-list__link landing-page-list__link_0 landing-page-list__link_13">
-                        <span class="landing-page-list__title">Обязательное страхование туриста</span>
-                    </a>
-                </div>
-            </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <a href="/life_insurance" class="landing-page-list__link landing-page-list__link_0 landing-page-list__link_3">
+                            <span class="landing-page-list__title">Страхование жизни</span>
+                        </a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="/travel_insurance" class="landing-page-list__link landing-page-list__link_0 landing-page-list__link_13">
+                            <span class="landing-page-list__title">Обязательное страхование туриста</span>
+                        </a>
+                    </div>
+                </div>-->
+            @endif
         </div>
     </div>
 </div>
---
+++ resources/views/layouts/modal/insurance.blade.php
@@ -0,0 +1,46 @@
+<div class="modal fade" style="font-size: 16px;" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content overflow-hidden">
+            <div class="product__img product__img__modal landing-page-list__link_3"></div>
+            <div class="modal-header border-0">
+                <h6 class="modal-title" id="exampleModalLongTitle">Страхование жизни</h6>
+            </div>
+            <div class="modal-body pt-0">
+                <table class="table table-bordered mb-0 table-striped">
+                    <tbody>
+                    <tr>
+                        <td scope="row">Страховая сумма</td>
+                        <td>1 000 000,00</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Год рождения</td>
+                        <td>1990</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Страховая премия</td>
+                        <td>10 000,00</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <section class="section-preview mt-3">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="defaultUnchecked">
+                        <label class="custom-control-label col-form-label login-remember" for="defaultUnchecked">Включить страховое покрытие от несчастного случая</label>
+                    </div>
+                </section>
+            </div>
+            <div class="modal-footer table-Active">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4">
+                            <button type="button" class="btn btn-secondary btn-login product-btn" data-dismiss="modal">Закрыть</button>
+                        </div>
+                        <div class="col-8">
+                            <button type="button" class="btn btn-primary btn-login product-btn">Перейти к заполнению анкеты</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
